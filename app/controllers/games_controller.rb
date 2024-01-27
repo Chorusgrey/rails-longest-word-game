1fa84a4cbceeead4ce7ef02ab2f4d68e43988f9b
Perrine Dohr <antartikat@gmail.com>
2024-01-27 16:54:56 +0100

    fait

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,32 @@
+require 'open-uri'
+require 'json'
 class GamesController < ApplicationController
 
   def new
-    @letters = ("a".."z").to_a.sample(10)
+    @letters = ("A".."Z").to_a.sample(10)
   end
 
   def score
-    @word = params[:word]
-    # recupere les lettres de la grille de lettre et bien utiliser les lettres de la grille
-    # params l'array des lettres
-    # params des mots
-    # if est ce que le mot il est bien construit par des lettres présentes
-    # donc if  le mot est le dictionnaire voir api
+    @word = params[:word].upcase.chars
+    @letters = params[:letters].split
+    if @word == []
+      @message = "Sorry you must write a word"
+    elsif included?(@word, @letters) && english_word?(@word.join)
+      @message = "You won, your word: #{@word.join}"
+    else
+      @message = "You loose!!!"
+    end
+  end
+
+  private
+
+  def english_word?(word)
+    response = URI.parse("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def included?(word,letters)
+    @word.all? { |letter| @word.count(letter) <= @letters.count(letter) }
   end
 end
